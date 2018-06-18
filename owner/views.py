3ee95e53c5e36fe3dcc738279d9c923c9557e17c
diptangsu <deep.dippu@gmail.com>
2018-06-18 22:51:31 +0530

    added login page

--- owner/views.py
+++ owner/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    # TODO: change this to index page with all login options
     return render(request, 'owner/login.html')
 
 
 def login(request):
-    if request.method == 'POST':
-        pass
-    else:
+    if request.method == 'GET':
         return render(request, 'owner/login.html')
+    else:
+        pass
