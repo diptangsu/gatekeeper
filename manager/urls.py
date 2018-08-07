821c99b582a7cbdce7270dcc03f0e4f6748ce693
diptangsu <deep.dippu@gmail.com>
2018-08-07 16:59:57 +0530

    added employee details upload page for managers

--- manager/urls.py
+++ manager/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name='manager-dashboard'),
     path('login', views.login, name='manager-login'),
     path('logout', views.logout, name='manager-logout'),
-    path('all-visitors', views.all_visitors, name='manager-all-visitors')
+    path('all-visitors', views.all_visitors, name='manager-all-visitors'),
+    path('add-employees', views.add_employees, name='manager-add-employees'),
+    path('manager-upload-csv', views.manager_upload_csv, name='manager-upload-csv')
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
-from gatekeeper.decorators import is_logged_in
+from django.core.files.storage import FileSystemStorage
+from django.utils import timezone
+
 from .models import Manager
 from reception.models import Visitor
+
+from gatekeeper.decorators import is_logged_in
 from datetime import datetime
-from django.utils import timezone
 import pusher
 
 
@@ -67,10 +70,16 @@ def all_visitors(request):
     manager_id = request.session.get('manager_id')
     manager = Manager.objects.get(id=manager_id)
 
-    today_min = datetime.combine(timezone.now().date(), datetime.today().time().min)
-    today_max = datetime.combine(timezone.now().date(), datetime.today().time().max)
+    if request.method == 'GET':
+        start_date = datetime.combine(timezone.now().date(), datetime.today().time().min)
+        end_date = datetime.combine(timezone.now().date(), datetime.today().time().max)
+    else:
+
+        start_date = datetime.combine(timezone.now().date(), datetime.today().time().min)
+        end_date = datetime.combine(timezone.now().date(), datetime.today().time().max)
+
     visitors = Visitor.objects.filter(company_to_visit=manager) \
-        .filter(in_time__range=(today_min, today_max)).order_by('-in_time')
+        .filter(in_time__range=(start_date, end_date)).order_by('-in_time')
 
     return render(request, 'manager/all-visitors.html',
                   {
@@ -78,3 +87,36 @@ def all_visitors(request):
                       'visitors': visitors,
                   })
 # TODO: get visitors on 
+
+
+@is_logged_in('manager')
+def add_employees(request):
+    manager_id = request.session.get('manager_id')
+    manager = Manager.objects.get(id=manager_id)
+
+    if request.method == 'GET':
+        return render(request, 'manager/add-employees.html',
+                      {
+                          'manager': manager,
+                      })
+    else:
+        raise Http404
+
+
+def add_students_from_csv(uploaded_file_url):
+    pass
+
+
+def manager_upload_csv(request):  # for ajax
+    if request.method == 'POST':
+        file = request.FILES.get('employee-details', None)
+        if file is None:
+            pass
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            uploaded_file_url = fs.url(filename)
+            add_students_from_csv(uploaded_file_url)
+
+    else:
+        raise Http404
